e33d4febcfbb4478412062336cbb55a2a4a4953e
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-08-14 22:43:28 +0900

    feat: User 모델 추가 - 필요할 거 같은 메소드 미리 추가 - 편하게 AbstractUser 상속하여 만듬

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from rest_framework.exceptions import NotFound
+
+
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+    def get_object(pk: int):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise NotFound
+
+    def create_test_list(n: int) -> list:
+        user_list = []
+        for i in range(n):
+            user = User.objects.create(
+                username="testuser" + str(i),
+            )
+            user.set_password("123")
+            user.save()
+            user_list.append(user)
+        return user_list

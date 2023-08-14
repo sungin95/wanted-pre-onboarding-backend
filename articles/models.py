d47ea03763e6b63411f4564809374f3950c5ff94
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-08-14 22:50:17 +0900

    feat: Articles 모델 생성 - owenr 생성자 - title 제목 - content 내용

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from common.models import CommonModel
+from accounts.models import User
+from rest_framework.exceptions import NotFound
+from django.db import transaction
+from rest_framework.exceptions import ParseError
 
-# Create your models here.
+
+# 공용 질문
+class Articles(CommonModel):
+    owner = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(
+        max_length=255,
+    )
+    context = models.TextField()
+
+    # 모델 관리
+    def get_object(pk: int):
+        try:
+            return Articles.objects.get(pk=pk)
+        except Articles.DoesNotExist:
+            raise NotFound

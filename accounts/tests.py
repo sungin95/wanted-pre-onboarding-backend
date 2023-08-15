83535e727f8ee2678e1ce71737c87b8f4589ff7a
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-08-15 09:59:07 +0900

    feat: accounts urls 추가 및 연결

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from accounts.models import User
 
-# Create your tests here.
+
+class TestCreateUser(APITestCase):
+    URL = "/api/v1/accounts/create"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import (
+    PermissionDenied,
+    ParseError,
+    NotFound,
+    NotAuthenticated,
+    ParseError,
+)
+from rest_framework import status
+from .models import User
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAuthenticated,
+)
+from config import settings
 
-# Create your views here.
+
+class UserGetCreate(APIView):
+    def get(self, request):
+        return Response(status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response(status.HTTP_201_CREATED)
